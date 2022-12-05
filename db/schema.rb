1296564080a69d2abad24e3ed15a08ccdf1ef358
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-05 19:05:12 +0000

    Add rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_05_184356) do
+ActiveRecord::Schema.define(version: 2022_12_05_190409) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "file"
   end
 
 end
