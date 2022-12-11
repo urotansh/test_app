54fd61cff0e23a36f9e610443ca61ac44224c38b
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-11 16:09:46 +0000

    Add video_file to StrongParameter of TweetsController

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -66,6 +66,6 @@ class TweetsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def tweet_params
-      params.require(:tweet).permit(:body, :audio_file)
+      params.require(:tweet).permit(:body, :audio_file, :video_file)
     end
 end
